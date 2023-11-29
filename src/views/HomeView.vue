89500be3846505f83022870adbb84120d5f0b2c9
justic <1131918725@qq.com>
2023-11-29 21:04:08 +0800

    feat: add blog paeg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watchEffect } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import usePageTitle from '@/hooks/usePageTitle'
 import { getBlogList } from '@/api/blogApi'
 import BlogCard from '../components/BlogCard.vue'
@@ -8,13 +8,20 @@ import BlogCard from '../components/BlogCard.vue'
 usePageTitle('首页')
 
 const $route = useRoute()
+const $router = useRouter()
 
 // 定义 list total
 const list = ref([])
 const total = ref(0)
 
+const currentPage = ref(1)
+const pageSizeRef = ref(10)
+
 // 使用watchEffect 监听url 参数 page pageSize category keyword 的变化，并获取博客列表和总数
 watchEffect(async () => {
+  currentPage.value = +$route.query.page || 1
+  pageSizeRef.value = +$route.query.pageSize || 10
+
   const queryInfo = {
     page: +$route.query.page || 1,
     pageSize: +$route.query.pageSize || 10,
@@ -26,10 +33,43 @@ watchEffect(async () => {
   list.value = blogList
   total.value = blogTotal
 })
+
+const handlePageChange = (newPage) => {
+  $router.push({
+    path: $route.path,
+    query: {
+      page: newPage,
+      pageSize: pageSizeRef.value
+    }
+  })
+}
 </script>
 
 <template>
   <div v-for="item in list" :key="item.id">
     <blog-card :blog="item"></blog-card>
   </div>
+
+  <div class="pagination-container">
+    <el-pagination
+      :total="total"
+      :page-size="pageSizeRef"
+      layout="prev, pager, next"
+      :currentPage="currentPage"
+      @current-change="handlePageChange"
+      class="pagination"
+    ></el-pagination>
+  </div>
 </template>
+
+<style scoped>
+.pagination-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.pagination {
+  margin-top: 10px;
+}
+</style>
